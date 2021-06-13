9da49fc5059f2ab6cff32b91c0f3b4a77aee319f
retro <84179847+retro-ai@users.noreply.github.com>
2021-06-13 11:01:58 +0100

    Add reference config file

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    token: 'your-token-here',
+    host: 'http://localhost:31415',
+    port: 31415,
+    recaptcha: {
+        siteKey: 'your-key-here',
+        secretKey: 'your-key-here',
+    },
+};
