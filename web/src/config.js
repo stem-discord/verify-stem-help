a35a53ef26aa199a4abf7fb386c8370c46459052
nopeless <38830903+nopeless@users.noreply.github.com>
2021-06-21 00:49:51 +0900

    create config and test script

---
+++ web/src/config.js
@@ -0,0 +1,9 @@
+
+// the configuration file
+// the settings here will get passed to the render function
+
+module.exports = {
+	index: {
+		work: "spongebob"
+	}
+}
--- web/src/js/scripts.js
+++ web/src/js/scripts.js
@@ -1,2 +1,4 @@
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+
+console.log("u suck")
