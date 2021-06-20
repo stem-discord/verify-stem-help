9ec4a062330dc1c24a7f22d1f966e38602bc8e33
nopeless <38830903+nopeless@users.noreply.github.com>
2021-06-21 01:44:51 +0900

    test config js

--- src/config.js
+++ src/config.js
@@ -1,9 +1,16 @@
 
 // the configuration file
 // the settings here will get passed to the render function
+const path = require('path');
 
 module.exports = {
-  index: {
-    work: 'spongebob',
+  // please use directory/name when doing this.
+  'dist': path.join(__dirname, '../dist'),
+
+  // injected into pug pages
+  'views': {
+    'index': {
+      work: 'spongebob',
+    },
   },
 };
