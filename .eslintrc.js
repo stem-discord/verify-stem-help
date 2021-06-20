6e59e400b88367f464c24c0d0688ff48427c8d6b
nopeless <38830903+nopeless@users.noreply.github.com>
2021-06-21 01:13:04 +0900

    add rules and update

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,9 +2,24 @@
 
 module.exports = {
   extends: ['eslint:recommended', 'google'],
+  parserOptions: {
+    emcaVersion: 2018,
+    ecmaVersion: 9,
+    sourceType: 'module',
+    ecmaFeatures: {
+      'jsx': true,
+      'experimentalObjectRestSpread': true,
+    },
+  },
   env: {
     browser: true,
     amd: true,
     node: true,
+    es6: true,
+  },
+  rules: {
+    'require-jsdoc': 0,
+    'max-len': 0,
+    'no-unused-vars': 1,
   },
 };
